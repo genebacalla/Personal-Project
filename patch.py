50c54993df48962751c65d79465e5bdf5645eca7
Song Bacalla <bueno.gene11@gmail.com>
2024-01-18 06:13:32 +0800

    Patch.py can now aggregate img tags associated with Buff

--- patch.py
+++ patch.py
@@ -4,27 +4,27 @@ from pyppeteer import launch
 import asyncio
 import os
 
+
+# <li><img alt = "">
+
+data_list = []
+
 url = "https://liquipedia.net/dota2/Version_7.35"
 resp = requests.get(url)
 html_content = resp.content
 
 soup = BeautifulSoup(html_content,'html.parser')
-with open("text/html_text.txt", "w", encoding="utf-8") as f:
-    f.write(str(soup))
 
+for li in soup.find_all('li'):
+    #img = li.find('img')
 
-mark = "<div class='content-ad-block navigation-not-searchable'>"
+    if(li.find('img',alt='Buff') or li.find('img',alt='Nerf')):
+        img = li.find('img',alt='Buff')
+        #a_tag = li.find("a")
 
-with open("text/html_text.txt", "r", encoding="utf-8") as f:
-    file_content = f.read()
+        if (img):
+            print(img)
+            print(li.text)
+            print("\n")
 
-start_index = file_content.find(mark)
-end_index = file_content.rfind(mark)
-
-if start_index != -1 and end_index != -1:
-    cut_content = file_content[start_index + len(mark):end_index].strip()
-
-    # Save the cut content to a new text file
-    with open("text/html_text.txt", "w", encoding="utf-8") as f:
-        f.write(cut_content)
-
+    
