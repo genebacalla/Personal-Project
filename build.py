3e6015c5948e68031f4b3826867e5a1cee8b165e
Song Bacalla <bueno.gene11@gmail.com>
2024-01-24 17:55:34 +0800

    Cleaned the code

--- build.py
+++ build.py
@@ -3,114 +3,107 @@ import json
 import preprocess
 import requests
 from pyppeteer import launch
+import time
 
 
 
-hero_name = ""
-patch_category=""
-patch_class=""
-patch_note=""
 
-dict_skill = {}
-dict_talent = {}
+lss=[]
 
-flag_stop = False
+hero = " "
+name = " "
+note = " "
+p_class = " "
 
-def dataset():
 
-    #7.30C is the start of proper formatting
 
-    global patch_category
-    global patch_class
-    global patch_note
+def getPatch():
 
-    ctr = 0
-    stop_flag =True
-    patch_versions = ["7.30c","7.31","7.31b","7.31c","7.31d","7.32","7.32b","7.32c","7.32d","7.32e","7.33","7.33b","7.33c","7.33d","7.34","7.34b","7.34c","7.34d","7.34e","7.35"]
-    alt_texts = ["Buff","Nerf","Rework","Rescale","Removed"]
+    global hero,name,note
 
-    # for version in patch_versions:
-
-    # if ctr < round((len(patch_versions)/2)+2):
-    #     data_path = "dataset/train"
-    # else:
-    #     data_path = "dataset/test"
-
-    # ctr+=1
+    patch_classes = ['Buff','Rework','Nerf','Rescale','Removed','New']
+    sentinel = 0
 
     url = f"https://liquipedia.net/dota2/Version_7.35"
     resp = requests.get(url)
     html_content = resp.content
 
     soup = BeautifulSoup(html_content,'html.parser')
+    all_h3 = soup.find_all('h3')
 
-
-    for h3_tag in soup.find_all('h3'):
-        ul_tag1 = h3_tag.find_next_sibling('ul')
-
-
-
-        if ul_tag1:
+    
+    
+    for i,h3 in enumerate(all_h3):
+        
+    
+        if (h3.find_next('ul')):
             
+            #print(h3.text.strip())
+            ul_container = h3.find_next('ul')
 
 
-            ul_soup1 = ul_tag1.find_all('li',recursive=False)
+            all_li = ul_container.find_all('li')
 
-            if ul_soup1:
-                print(h3_tag.text.strip())
-
-                for li_tag1 in ul_soup1:
-           
-        
-                    ul_soup2 = li_tag1.find("ul")
+            for i,li in enumerate(all_li):
                     
-                    if (ul_soup2):
+                in_line = li.find('b')
 
-                        li_soup = li_tag1.find_all('li')
-                        category = li_tag1.find('b')
-
-                        if category:
-                            patch_category = category.text.strip()
-                            print(patch_category)
-                    
-   
-                        
-                        for li_tag2 in li_soup:
-                           
-                            imgAlt_tag = li_tag2.find('img',alt=lambda value: value in alt_texts)
-                
-                        
-
-                            if imgAlt_tag:
-                                print(imgAlt_tag.get('alt')+" "+li_tag2.text.strip())
-                                patch_class = imgAlt_tag.get('alt')
-                                patch_note = li_tag2.text.strip()
-                                stop_flag=False
-
-                            else:
-                                if imgAlt_tag == None:
-                                    print("None detected")
-
-                
+                if (in_line):
+                    #print(in_line.text.strip())
+                    lss.append(in_line.text.strip())
+               
+                else:
+                    patch_class = li.find('img',alt=lambda value: value in patch_classes)
+                    if (patch_class):
+                        #print(patch_class.get('alt') + " " + li.text.strip())
+                        lss.append(patch_class.get('alt') + " " + li.text.strip())
+          
                     else:
-                        imgAlt_tag = li_tag1.find('img',alt=lambda value: value in alt_texts)
-                        if imgAlt_tag:
-                            print("LOWER")
-                            print(imgAlt_tag.get('alt')+" "+li_tag1.text.strip())
-                            stop_flag = False
-                            patch_class = imgAlt_tag.get('alt')
-                            patch_note = li_tag1.text.strip()
+                        sentinel=1
 
-    
-        if (stop_flag):
-            continue
-        else:
+        # if (sentinel==1):
+        #     print("WALANG B AT WALANG PATCH CLASS!")
+        #     sentinel=0
+        # else:
+        print(h3.text.strip())
+        for values in lss:
+            print(values)
 
-            stop_flag = True
-    
+        print("\n")
+        lss.clear()
+
+    #
 
 
-  
-        
+         
 
-dataset()
+    # STRUCTURE OF HEROES, SKILL NAME, AND ADJUSTMENTS MADE
+    # <h3> - Hero name 
+    # <ul> - patch container
+    #   <li>
+    #       <img alt=""> -> in-line note, can print the whole list immediately 
+    #   <li>
+    #       <b> - Skill name, must be followed up by <ul> below
+    #       <ul> 
+    #           <li>
+    #               <img alt = ""> -> in-line note again, can print the whole list immediately
+    #   <ul>
+    #       <li>
+    #       <li>
+    #       <li> 
+
+    # Sibling <ul> tags should be analyzed base on its <li> childs. 
+    # If a <li> child contains an <img alt = "">, the it is an in-line text, wherein the patch class
+    # is placed next to the note. 
+    # If a <li> child contains an <b>, then its a skill / talent / shard title, wherein the <ul> tag next to 
+    # it is used as a container. All list inside that container is in-line similar to the first example.  
+
+
+    # There should be a function that accepts a <li> tag and analyzed it if its an immediate in-line
+    # or secondary in-line. If immediate, then return the patch_class + patch_note immediately. If not
+    # then fetch the <b> tag to get the skill/shard/talent name and analyze the <ul> again. 
+
+
+
+
+getPatch()
